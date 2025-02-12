0d0931f471d6a5a00ff4c253573c0eeeacf9d99c
DSA <mayurisharma2003@gmail.com>
2025-02-12 12:52:21 +0530

    fix error

--- server.js
+++ server.js
@@ -74,6 +74,6 @@ app.post('/search', (req, res) => {
   
 
 // Listen on port 3000
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+app.listen(4000, () => {
+  console.log('Server running on http://localhost:4000');
 });
